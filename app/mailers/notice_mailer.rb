5afdf4c99ef3e91aba0f2b3d4675cc5f7c48b447
USER <user@kohei.local>
2017-07-02 16:13:11 +0900

    “修正”

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -2,14 +2,14 @@ class NoticeMailer < ApplicationMailer
   def sendmail_blog(blog)
     @blog = blog
 
-    mail to: "@blog.user.email",
+    mail to: @blog.user.email,
          subject: '【Achieve】ブログが投稿されました'
   end
 
   def sendmail_contact(contact)
   @contact = contact
 
-      mail to: "@contact.email",
+      mail to: @contact.email,
        subject: '【Achieve】お問い合わせが完了しました'
   end
 end
