450248f09812aa22b190d58fc3726304df7b7925
USER <user@kohei.local>
2017-08-06 21:27:03 +0900

    メッセージ機能修正完了

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,6 +5,13 @@ class ConversationsController < ApplicationController
   end
 
   def create
+    #送ったユーザーと受け取ったユーザーに関係性があるか？を判定
+    #例：
+    #松本 => 金井　メッセージを送信
+    # 松本 => sender_idに松本さんのユーザーIDが設定される
+    # 金井 => recipient_idに自分のユーザーIDが設定される
+    # idは違うけど、二人の会話は成立しているため関係性がある
+    #よってif文ではtrueを返す
     if Conversation.between(params[:sender_id], params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
     else
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,6 +23,38 @@ class MessagesController < ApplicationController
     @message = @conversation.messages.build
   end
 
+  def create
+    #紐付いているものに対してbuildでインスタンスを生成する
+    # @message = @conversation.messages.new(message_params)
+
+
+
+    #***** Message.new(message_params)を使用した場合(正しくない実装)******
+    #conversation_idはもともと設定されており、
+    #紐付いたメッセージを作成する(message_paramsにはmessage_idが設定される)
+    #conversation_idをひも付けようとするがMessageテーブルにconversation_idの
+    #カラムは存在しない、
+    #Message.new(message_params)をすると会話に紐付いた値を取得できない
+    #また紐付いた会話に対するメッセージIDが作成できない。(INSERTが失敗する)
+    #つまりDBエラーが発生する。
+    #結果としてPOSTのredirectのアクションが実行される
+    #つまりリクエスト先はmessages#createとなる
+    #@message = Message.new(message_params)
+
+    #@message = @conversation.messages.new(message_params)を使用した場合(正しい実装)
+    #会話に紐付いたインスタンスを生成する。
+    #@message = @conversation.messages.new(message_params)
+    #上記1文の実行イメージとしては入力フォームした箱が作成される
+    #入力した情報をsaveする。insertが成功する
+    #成功してGETアクションが実行される
+    #つまりmessage#indexがリクエスト先となる
+    @message = @conversation.messages.new(message_params)
+
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    end
+  end
+
   private
   def message_params
     params.require(:message).permit(:body, :user_id)
