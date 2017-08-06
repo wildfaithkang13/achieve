f70b3c17e73071f5eb4c219be979e1a58efcf5e3
USER <user@kohei.local>
2017-08-06 16:38:16 +0900

    メッセージ機能修正

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,20 +1,36 @@
-class ConversationsController < ApplicationController
-  def index
-    @users = User.all
-    @conversations = Conversation.all
+class MessagesController < ApplicationController
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
   end
 
-  def create
-    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
-      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
-    else
-      @conversation = Conversation.create!(conversation_params)
+  def index
+  @messages = @conversation.messages
+  if @messages.length > 10
+    @over_ten = true
+    @messages = @messages[-10..-1]
+  end
+
+  if params[:m]
+    @over_ten = false
+    @messages = @conversation.messages
+  end
+
+  if @messages.last
+    if @messages.last.user_id != current_user.id
+     @messages.last.read = true
     end
+  end
+
+  @message = @conversation.messages.build
+
+  def create
+  @message = @conversation.messages.build(message_params)
+  if @message.save
     redirect_to conversation_messages_path(@conversation)
   end
 
   private
-    def conversation_params
-      params.permit(:sender_id, :recipient_id)
-    end
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
 end
